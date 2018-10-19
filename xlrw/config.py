7c9ce01ce97660c05cc3a5c1df9fb76085c672d9
kowkanonk <cd8866@qq.com>
2018-10-19 16:26:27 +0500

    Create config.py

---
+++ xlrw/config.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+#
+# Copyright 2018 by weixl. All Rights Reserved.
+#
+# Permission to use, copy, modify, and distribute this software and its
+# documentation for any purpose and without fee is hereby granted,
+# provided that the above copyright notice appear in all copies and that
+# both that copyright notice and this permission notice appear in
+# supporting documentation, bulabula...
+#
+# For the change history, see README.txt in the distribution.
+#
+# This file is part of the xxxxx. See
+# http://xxxx
+#
+
+"""
+Template configuration file for Python Project.
+Solution for small applications.
+Copyright (C) 2018 weixl. All Rights Reserved.
+To use, simply 'import config' and refer by 'config.COMMON.LOG_CONFIG'!
+"""
+
+# import os
+
+#---------------------------------------------------------------------------
+#   Miscellaneous module configuration
+#---------------------------------------------------------------------------
+
+class COMMON:
+    """
+    General congiruations of project. Such as log config file, 
+    
+    """
+    LOG_CONFIG = './logger.yaml'
+    
+#---------------------------------------------------------------------------
+#   Something else
+#---------------------------------------------------------------------------
+
+class SOMETHING:
+    """
+    Something else
+    
+    """
